0e19b8b3249ca039816536d474d47da3906f050e
Łukasz Strzyżewski <l.j.strzyzewski@gmail.com>
2017-09-14 15:05:08 +0000

    Changed the greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"),
       app     = express();
       
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
+  res.send({bye: 'buddy'});
 });
 
 app.listen(process.env.PORT, process.env.IP)
